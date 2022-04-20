eb72cbac86999bfeb43330a121d3f6ab031fa02d
jerosam2002 <78993616+jerosam2002@users.noreply.github.com>
2022-04-20 22:01:53 +0530

    some change

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import os
+os.system('python face_recog/faceID.py')
 #HaarCascade
 os.system('python emotions.py --mode display')
 #MTCNN
 #os.system('python emo.py --mode display')
-os.system('python face_recog/faceID.py')
